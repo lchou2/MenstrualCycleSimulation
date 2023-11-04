b7cda27305c88ff407e87b934b593815cd816441
anyaelissa <schwanesa@sfusd.edu>
2023-11-04 10:00:32 -0700

    added display window

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ pygame.init()
 # Window dimensions
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
+
+# Create the window
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Menstrual Cycle Simulation")
+window.fill((255, 255, 255))
+
+# Dimensions of the sprites
+CL_LARGE_WIDTH = 30
