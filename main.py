d5f00a45dd5a4313254fec595a6b444f03e52c75
lchou2 <116858972+lchou2@users.noreply.github.com>
2023-11-04 09:50:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ pygame.init()
 # Window dimensions
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
+
+# Create the window
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption("Menstrual Cycle Simulation")
+
+
+class Hormone(pygame.sprite.Sprite):
+    def __init__(self, image, rect, speed):
+        super().__init__()
+        self.image = image
+        self.rect = rect
+        self.mask = pygame.mask.from_surface(image)
+        self.speed = speed
