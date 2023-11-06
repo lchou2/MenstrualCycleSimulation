5cfab0f8fc093ffc0b93ad780eff295fb46ab47c
lchou2 <116858972+lchou2@users.noreply.github.com>
2023-11-05 18:18:12 -0800

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,19 @@ WINDOW_HEIGHT = 1080
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("Menstrual Cycle Simulation")
 
-
+#Not yet functional.  Hormones will function similar to projectiles in our evil cluthes game.
 class Hormone(pygame.sprite.Sprite):
     def __init__(self, image, rect, speed):
-        super().__init__()
         self.image = image
         self.rect = rect
         self.mask = pygame.mask.from_surface(image)
         self.speed = speed
+
+#Not yet functional.  Beginning outline for moving the follicles.  Plan to make the sliders a sub class.
+class Follicle(pygame.sprite.Sprite):
+    def __init__(self,image,rect):
+        self.image = image
+        self.rect = rect
+        self.mask = pygame.mask.from_surface(image)
+
+
