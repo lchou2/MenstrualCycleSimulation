a95b807ce9092135eb59f8164409eacc5fdebd92
lchou2 <116858972+lchou2@users.noreply.github.com>
2023-11-12 10:46:28 -0800

    Began to define hormone class

--- Learning mode.py
+++ Learning mode.py
@@ -163,7 +163,7 @@ class Slider(pygame.sprite.Sprite):
 CL_LARGE_WIDTH = 30
 
 def main():
-    lhslider = Slider(LH_slider_image,(920, 420), (20,20), 0, 420, 300)
+    lhslider = Slider(LH_slider_image,(920, 420), (20,20), 0, 420, 340)
     slider_group.add(lhslider)
 
     running = True
